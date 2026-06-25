8c713be7b7616a53a1c4835d370994716747eadc
Suryaprabha1030 <suryachandrasekar10@gmail.com>
2026-06-25 23:11:24 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
     setSession(null);
     setView("home");
   }
-
+  console.log("Has key:", !!process.env.supabase_key, process.env.supabase_key);
   return (
     <>
       <Navbar
